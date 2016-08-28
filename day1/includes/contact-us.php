bf32ca8a413669cd485c4a07c61d6f7e716e45c3
muratkilic1978 <muratkilic1978@gmail.com>
2016-08-28 21:02:28 +0200

    Added PHP include project folder
    This folder contains an example that is showing how you can use the
    include() function in PHP.

---
+++ day1/includes/contact-us.php
@@ -0,0 +1,20 @@
+<!-- PAGE HEADER -->
+<?php
+	
+	$pageTitle = 'Contact Us';
+	include('includes/header.php');
+	
+?>
+<!-- PAGE CONTENT -->
+<h2>Contact Us</h2>
+<h3 class="address-heading">Address</h3>
+<p class="address">TLA Accounting Services Inc.</p>
+<p class="address">2655 Mary Hill Road, #103</p> 
+<p class="address">Port Coquitlam, BC V3C 6R9 Canada</p>
+
+<!-- PAGE FOOTER -->
+
+<?php
+	include('includes/footer.php');
+
+?>
---
+++ day1/includes/includes/footer.php
@@ -0,0 +1,14 @@
+<table width="100%" bgcolor="#000000" cellpadding="12" border="0">
+<tr>
+	<td>
+    	<p class="foot">&copy; TLA Consulting Services Inc.</p>
+    	<p class="foot">2655 Mary Hill Road, #103&nbsp; <?php echo date('Y/m/'); ?></p>
+        <p class="foot">Port Coquitlam, BC V3C 6R9 Canada</p>
+        <p class="foot">Please see our <a class="cta" href="#">legal information page</a></p>
+    </td>
+</tr>
+</table>
+
+
+</body>
+</html>
