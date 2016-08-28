a651c460b8efa8449e41884d72f6d93dc4c10520
muratkilic1978 <muratkilic1978@gmail.com>
2016-08-28 22:09:01 +0200

    Added write form-data to text file project
    This project contains PHP source-codes - where it is possible to submit
    your fav bookmark to a text file.

---
+++ day1/add-form-data-to-text-file/addtofile.php
@@ -0,0 +1,45 @@
+
+	<html>
+	<head>
+	<title>Write to a text file</title>
+	</head>
+	<body>
+
+	<h1>Adding your bookmarks to a text file:</h1>
+	<form action="addtofile.php" method='post'>
+		<fieldset>
+			<legend>Add bookmark</legend>
+			<p><label class="field" for="Description">Description:</label><input type="text" name="description" value="" ></p>
+			<p><label class="field" for="URL">URL:</label><input type="url" name="webadress" value="" ></p>
+			
+			<input type='submit' value='Add text'>
+		</fieldset>
+	</form>
+
+	<?php
+
+	// Open the text file
+	$f = fopen("textfile.txt", "a+");
+
+	$textblock = $_POST['description'] . ", " . $_POST['webadress'] . "\n";
+
+	// Write text
+	fwrite($f, $textblock); 
+
+	// Close the text file
+	fclose($f);
+
+	// Open file for reading, and read the line
+	$myfile = fopen("textfile.txt", "r") or die("Unable to open file");
+
+	while (!feof($myfile)) {
+		echo fgets($myfile) . "<br>";
+	}
+
+	fclose($myfile);
+
+	?>
+	
+	</body>
+
+	</html>
